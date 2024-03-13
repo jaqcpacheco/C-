0b68ac043937605684911bdf45775e16030d5ec1
jaqcpacheco <127060044+jaqcpacheco@users.noreply.github.com>
2024-03-12 23:08:47 -0300

    Add files via upload
    TDE 1 C# Laboratório de Programação

---
+++ TDE 1 LP/Program.cs
@@ -0,0 +1,55 @@
+﻿// 1- Crie uma variável chamada idade e atribua a ela um valor 35 e exiba seu valor
+int idade = 35;
+Console.WriteLine(idade);
+
+// 2- Crie uma variável chamada nome a atribua o valor "Maria" e exiba seu valor
+string nome = "Maria";
+Console.WriteLine(nome);
+
+// 3- Crie uma variável chamada altura e atribua a ela o valor 3.45
+double altura = 3.45;
+Console.WriteLine(altura);
+
+// 4- Crie uma constante chamada ano e atribua a ela o valor 12 e exiba o seu valor
+const double ano = 12;
+Console.WriteLine(ano);
+
+// 5- Declare uma variável nota do tipo double como sendo do tipo Nullable type e atribua o
+//valor 7.80 e exiba o seu valor
+double? nota = null;
+nota = 7.80;
+Console.WriteLine(nota);
+
+// 6- Quais as diferenças entre os tipos por valor e os tipos por referência ?
+/* Tipos de valor armazenam diretamente seus valores nos dados, tipos de referência armazenam referências em seus dados.*/
+
+// 7- O que é um nullable type e qual a sua utilidade ?
+/* Nullable type é uma instância da estrutura genérica, é usado para um tipo de 
+valor anulável quando precisa representar o valor indefinido de um tipo de valor subjacente*/
+
+// 8- O que é Camel Case ? Dê um exemplo de sua aplicação.
+/* Camel Case é uma combinação de palavras em uma palavra só, por exemplo quando vamos programar precisamos atribuir nomes
+as variáveis, então para atribuir mais de duas palavras sem espaço, um dos jeitos de se nomear em uma palavra só é assim nomeProfessor.*/
+
+// 9- O que é Pascal Case ? Dê um exemplo de sua aplicação
+/* Pascal Case é uma combinação de palavras em uma só, por exemplo YouTube, que é utilizada maiúscula no início de cada palavra.*/
+
+// 10- Declare duas variáveis x e y como sendo do tipo int e atribua a ela os valores 77 e 66 e a
+//seguir imprima o valor da soma de x com y. (Use o operador aritmético + para realizar a
+//operação de soma)
+
+int x = 77;
+int y = 66;
+Console.WriteLine(x + y);
+
+// 11- Quais os valores padrões dos tipos de dados bool, char, int, double, float, decimal e string.
+
+/*
+bool - False
+char - '\0'
+int - -2.147.483.648 a 2.147.483.647
+double - 0
+float - 0
+decimal - 0 
+string - null
+*/
